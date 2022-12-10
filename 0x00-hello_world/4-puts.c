a3ba495944982bf7aff1d7316a27c8a0a03ac775
erhmerd <ahmadibrahimkhalil112@gmail.com>
2022-12-10 03:15:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - print a given phrase
  * return:0 if exited properly, non-zero otherwise
- */
+*/
+
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
 return (0);
+
 }
