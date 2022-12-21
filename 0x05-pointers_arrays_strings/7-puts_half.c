80b8b8b1f6e87274552cbb0ee96907ee62244b48
erhmerd <ahmadibrahimkhalil112@gmail.com>
2022-12-20 16:41:40 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * strlen - return the lenth of a string
+ *@s: string
+ * return: string
+ */
+int strlen(char *s)
+{
+int lomgi = 0;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
+}
